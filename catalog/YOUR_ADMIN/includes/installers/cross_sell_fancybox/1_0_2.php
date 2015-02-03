2c997bf1d8dcd7cc5cd635788845c740f9e250a0
Larry Bislew <bislewl@gmail.com>
2015-02-02 21:14:43 -0600

    added autoinstaller

--- catalog/YOUR_ADMIN/includes/installers/cross_sell_fancybox/1_0_2.php
+++ catalog/YOUR_ADMIN/includes/installers/cross_sell_fancybox/1_0_2.php
@@ -1,7 +1,2 @@
 <?php
-
 //fix bug in uninstall.sql
-
-$db->Execute("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '1.0.2' WHERE configuration_key = 'CSFB_VERSION' LIMIT 1;");
-  
-$messageStack->add('Installed Cross Sell Fancybox v1.0.2', 'success');
